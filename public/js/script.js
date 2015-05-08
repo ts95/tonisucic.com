da245aa75d8b6e4cd48255b598db0746de3f8d0b
Toni S <TeKanne774@gmail.com>
2015-05-08 18:54:48 +0200

    Add more mobile optimisations

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,8 @@
 $(function() {
 	var dotGenerator = new DotGenerator(0.5, $('#home'));
 	dotGenerator.generateDots();
+
+	$('.menu-icon').click(function() {
+		$('.navbar-links').slideToggle('fast');
+	});
 });
